c64fcc4abb7df314e05685b22d5e7ce808752c27
wangqi <1015350043@qq.com>
2022-06-05 16:05:53 +0800

    tailwind @apply

---
+++ src/Tailwind.tsx
@@ -0,0 +1,9 @@
+const Tailwind = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <button className="btn-blue m-4">button</button>
+    </div>
+  )
+}
+
+export default Tailwind
